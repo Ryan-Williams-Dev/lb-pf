19a950c0b4c387737c97424bae8ed51fe6671e0d
Ryan Williams <r.d.williams.ca@gmail.com>
2023-06-20 17:09:43 -0700

    made card fit to center of screen better

--- components/Work.tsx
+++ components/Work.tsx
@@ -10,13 +10,13 @@ function Work({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex relative overflow-visable flex-col text-left md:flex-row px-10 justify-evenly mx-auto items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-standard-text text-2xl">
         Work
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-main/20 scrollbar-thumb-highlight/80">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory px-[16vw] gap-[16vw] scrollbar-thin scrollbar-track-main/20 scrollbar-thumb-highlight/80">
         <WorkCard id="1" />
         <WorkCard id="2" />
         <WorkCard id="3" />
--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -13,6 +13,14 @@ function WorkCard({ id }: Props) {
     }
   };
 
+  const imageUrls = [
+    "https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894",
+    "https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894",
+    "https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894",
+    "https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894",
+    "https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894",
+  ];
+
   return (
     <article
       onClick={handleClickScroll}
@@ -24,10 +32,8 @@ function WorkCard({ id }: Props) {
         items-center 
         space-y-7 
         flex-shrink-0
-        w-[300px]
-        sm:w-[500px] 
-        md:w-[600px] 
-        xl:w-[900px]
+        min-w-[68vw]
+        max-w-[68vw]
         h-[500px]
         sm:h-[500px] 
         md:h-[600px] 
@@ -36,7 +42,7 @@ function WorkCard({ id }: Props) {
         bg-main/20 
         p-10 
         hover:opacity-100 
-        opacity-40 
+        opacity-90 
         cursor-pointer 
         transition-opacity 
         duration-200 
@@ -56,23 +62,10 @@ function WorkCard({ id }: Props) {
         exercitationem fugiat fuga est sapiente quam quod. Asperiores maxime
         quod perferendis?
       </p>
-      <div className="">
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
-        <img src="https://cdnb.artstation.com/p/assets/images/images/038/682/967/large/larissa-baroboskin-shooter-girl.jpg?1623770894" />
+      <div className="flex flex-row gap-5 flex-wrap">
+        {imageUrls.map((url, i) => {
+          return <img key={i} className="max-h-56" src={url} />;
+        })}
       </div>
     </article>
   );
